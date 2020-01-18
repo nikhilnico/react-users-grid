d8f8a3d4c06478b796a169d52e2b3471f33a427d
Nitin Kumar <nikhilnico@gmail.com>
2020-01-18 15:59:23 +0530

    added gh pages static site

---
+++ precache-manifest.2bcd6bfb887a37d2431f0d0da7b0b9c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be176ac0244d35c1fe454d9c7103dec6",
+    "url": "/react-users-grid/index.html"
+  },
+  {
+    "revision": "9f50047a97455616d8e3",
+    "url": "/react-users-grid/static/css/main.745fff78.chunk.css"
+  },
+  {
+    "revision": "259d563277cb58ddabfc",
+    "url": "/react-users-grid/static/js/2.dea7ba4a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-users-grid/static/js/2.dea7ba4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f50047a97455616d8e3",
+    "url": "/react-users-grid/static/js/main.8f4d3add.chunk.js"
+  },
+  {
+    "revision": "d11a2d6a95543232b638",
+    "url": "/react-users-grid/static/js/runtime-main.6461d899.js"
+  }
+]);
